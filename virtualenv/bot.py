c16f6cea639793d6226f20134b6b82ae3f04b1d9
Yaroslav <42324948+jeneveost@users.noreply.github.com>
2022-05-30 09:31:39 +0300

    Update bot.py

--- virtualenv/bot.py
+++ virtualenv/bot.py
@@ -4,8 +4,7 @@ import telebot
 from telebot.types import Message
 import random
 
-TOKEN = '812682071:AAEdrHVoch6Y0rUgA-zMrPAg7pjXfB2hCEM'
-bot = telebot.TeleBot(TOKEN)
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
